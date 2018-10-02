f6b3545c00ff4322fe536db95e69482c7ad73705
agungcahya <agungcahyailhami@gmail.com>
2018-10-02 21:29:52 +0700

    case jenis kendaraan

--- src/Kendaraan.java
+++ src/Kendaraan.java
@@ -1,7 +1,24 @@
 public class Kendaraan {
     String jenis;
-    
+
     Kendaraan(String jenis){
         this.jenis = jenis;
+    }
+
+    public void cek(){
+        switch (jenis){
+            case "Motor":
+                System.out.println("- Area Parkir motor");
+                System.out.println("- Area Parkir mobil");
+                System.out.println("- Area Parkir bus");
+                break;
+            case "Mobil":
+                System.out.println("- Area Parkir mobil");
+                System.out.println("- Area Parkir bus");
+                break;
+            case "Bus":
+                System.out.println("- Area Parkir bus");
+                break;
+        }
     }
 }
